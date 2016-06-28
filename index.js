8586e9d12e3fc55f7ac4c699cc557290a56feb8f
Jieyi Hu <fullstackpug@icloud.com>
2016-06-28 16:09:38 -0500

    AUS-29: 0.0.9 Test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+var path = require('path');
 module.exports = {
-    chrome_launcher: require('karma-chrome-launcher'),
-    firefox_laucher: require('karma-firefox-laucher'),
-    phantomjs_launcher: require('karma-phantomjs-launcher')
+    chrome_launcher: require('karma-chrome-launcher') || require(path.resolve(__dirname, './node_modules/karma-chrome-launcher/index')),
+    firefox_laucher: require('karma-firefox-launcher') || require(path.resolve(__dirname, './node_modules/karma-firefox-launcher')),
+    phantomjs_launcher: require('karma-phantomjs-launcher') || require(path.resolve(__dirname, './node_modules/karma-phantomjs-launcher'))
 }
