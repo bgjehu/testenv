04e0dcccbf450d04d3fdb1dcfe155431981bdefa
Jieyi Hu <fullstackpug@icloud.com>
2016-06-28 21:12:21 -0500

    AUS-29: Added partial comment in the source code

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/*
+    Note:	there are two kinds config file. First one is the test config file, created by user manually or thru CLI to specify how they want their tests to be run.
+            usually with name '***.config.js'. The second one is the testevn config file. It is named testenv.json in the root, work like a package.json. It used to
+            specify the setting of testenv.
+*/
+
 var path = require('path');
 module.exports = {
     chrome_launcher: require('karma-chrome-launcher') || require(path.resolve(__dirname, './node_modules/karma-chrome-launcher/index')),
