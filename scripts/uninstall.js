449a2b5d78c4781ad426bc050f61a1c0ccc89315
Jieyi Hu <fullstackpug@icloud.com>
2016-07-05 18:54:34 -0500

    AUS-29: sudo fix

--- scripts/uninstall.js
+++ scripts/uninstall.js
@@ -1,12 +1,10 @@
+const colors = require('colors');
 function uninstall(app) {
     const fs = require('fs');
     var paths = process.env.PATH.split(':');
-    if (process.getuid() === 0) {
-        for (var i=0; i< paths.length; i++) {
-            fs.unlink(`${paths[i]}/${app}`, ()=>{});
-        }
-    } else {
-        console.log(`Please uninstall ${app} with root: sudo npm un -D testenv`);
+    for (var i=0; i< paths.length; i++) {
+        fs.unlink(`${paths[i]}/${app}`, ()=>{});
     }
 }
+console.log('Using sudo to uninstall command line...'.yellow);
 uninstall('testenv');
