5d431f3343c2016d3e333e207e75d3d89b81f890
Jieyi Hu <fullstackpug@icloud.com>
2016-07-06 15:12:42 -0500

    AUS-29: expose all first level dependencies

--- index.js
+++ index.js
@@ -5,15 +5,15 @@
 */
 
 const path = require('path');
-const modules = ['karma-chrome-launcher', 'karma-firefox-launcher', 'karma-phantomjs-launcher', 'karma-jasmine', 'karma-browserify', 'browserify-istanbul', 'karma-coverage'];
 const _ = require('lodash');
+const dependencies = _.keys(require('./package.json').dependencies);
 var frameworks = _.zipObject(
     //  keys
-    _.map(modules, function (m) {
+    _.map(dependencies, function (m) {
         return m.split('-').join('_');
     }),
     //  values
-    _.map(modules, function (m) {
+    _.map(dependencies, function (m) {
         return require(m) || require(path.resolve(__dirname, `./node_modules/${m}`));
     })
 );
