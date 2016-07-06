98f309785fc820ceaf666b6183f54b16e07a48cf
Jieyi Hu <fullstackpug@icloud.com>
2016-07-05 19:32:52 -0500

    AUS-29: 0.1.7-b

--- scripts/install.js
+++ scripts/install.js
@@ -2,10 +2,9 @@ const colors = require('colors');
 function install(app) {
     const path = require('path');
     const fs = require('fs');
-    var localBin, globalBin, scriptCode;
-    localBin = path.resolve(__dirname, `../bin/${app}`);
+    var globalBin, scriptCode;
     globalBin = `/usr/local/bin/${app}`;
-    scriptCode = `#!/usr/bin/env node\nrequire('${localBin}')();`;
+    scriptCode = `#!/usr/bin/env node\nrequire('${__dirname}/../lib/cli/index')();`;
     fs.writeFile(globalBin, scriptCode, {mode:0o755}  , function(err){
         if (err) throw err;
         console.log(`${app} installed in ${globalBin}`.green); 
